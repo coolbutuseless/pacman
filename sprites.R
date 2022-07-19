89b0109e3b4a1aa7425706fdba663abb78918862
mike <mikefc@coolbutuseless.com>
2022-07-19 15:40:58 +1000

    Extract nativeraster sprites from spritemap

---
+++ sprites.R
@@ -0,0 +1,89 @@
+
+library(grid)
+library(nara)
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Load the spritemap for pacman and the ghosts
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+spritemap <- png::readPNG("image/game-sprites.png")
+
+if (FALSE) {
+  dim(spritemap)
+  grid.raster(spritemap)
+}
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' Extract a sprite from the spritemap
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+extract <- function(row, col) {
+  sprite <- spritemap[1 + (row-1)*16 + 0:15, 457 + (col-1)*16 + 0:15,]
+  
+  alpha <- sprite[,,1] == 1 | sprite[,,2] == 1 | sprite[,,3] == 1 
+  alpha[] <- as.numeric(alpha)
+  
+  new <- c(sprite, alpha)
+  d <- dim(sprite)
+  d[3] <- 4
+  dim(new) <- d
+  
+  nara::array_to_nr(new)
+}
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Extract pacman sprites
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+pacman <- list(
+  right = list(extract(1, 1), extract(1, 2), extract(1, 3), extract(1, 2)),
+  left  = list(extract(2, 1), extract(2, 2), extract(1, 3), extract(2, 2)),
+  up    = list(extract(3, 1), extract(3, 2), extract(1, 3), extract(3, 2)),
+  down  = list(extract(4, 1), extract(4, 2), extract(1, 3), extract(4, 2))
+)
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Extract ghost sprites
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ghost1 <- list(
+  right = list(extract(5, 1), extract(5, 2)),
+  left  = list(extract(5, 3), extract(5, 4)),
+  up    = list(extract(5, 5), extract(5, 6)),
+  down  = list(extract(5, 7), extract(5, 8))
+)
+
+ghost2 <- list(
+  right = list(extract(6, 1), extract(6, 2)),
+  left  = list(extract(6, 3), extract(6, 4)),
+  up    = list(extract(6, 5), extract(6, 6)),
+  down  = list(extract(6, 7), extract(6, 8))
+)
+
+ghost3 <- list(
+  right = list(extract(7, 1), extract(7, 2)),
+  left  = list(extract(7, 3), extract(7, 4)),
+  up    = list(extract(7, 5), extract(7, 6)),
+  down  = list(extract(7, 7), extract(7, 8))
+)
+
+ghost4 <- list(
+  right = list(extract(8, 1), extract(8, 2)),
+  left  = list(extract(8, 3), extract(8, 4)),
+  up    = list(extract(8, 5), extract(8, 6)),
+  down  = list(extract(8, 7), extract(8, 8))
+)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Combine all ghosts
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ghost <- list(
+  ghost1, ghost2, ghost3, ghost4
+)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Test plot of a single sprite
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+if (FALSE) {
+  grid.raster(ghost4$right[[1]], interpolate = FALSE)
+}
+
